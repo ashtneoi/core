c3f7cb0ab3bd7bda1bb5f49e6bd530a0ec3f18bb
ashtneoi <ashtneoi@gmail.com>
2016-02-27 00:16:43 -0800

    Add files from other places

---
+++ common.h
@@ -0,0 +1,5 @@
+#pragma once
+
+
+#define E_COMMON 1
+#define E_RARE 2
---
+++ prog.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+#include <stdlib.h>
+
+#include "fail.h"
+
+
+int main()
+{
+    fatal(E_COMMON, "Program not implemented");
+}
