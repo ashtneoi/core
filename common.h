9b24aed9dfbbea2cd60ff2d3a44d257c3ae9a8eb
ashtneoi <ashtneoi@gmail.com>
2016-04-11 18:40:21 -0700

    Add argument handler; test some fail.h functions

--- common.h
+++ common.h
@@ -1,8 +1,12 @@
 #pragma once
 
 
+#define _POSIX_C_SOURCE 2
+
+
 #define E_COMMON 1
 #define E_RARE 2
+#define E_USAGE 3
 
 
 #define print(x) fputs((x), stdout)
--- prog.c
+++ prog.c
@@ -1,12 +1,69 @@
 #include "common.h"
 
+#include <stdbool.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "fail.h"
 
 
-int main()
+int verbosity = 0;
+
+
+struct args {
+    int dummy;
+};
+
+
+void exit_with_usage()
 {
-    warning("About to exit");
-    fatal(E_COMMON, "Program not implemented");
+    print(
+        "Usage:  prog [OPTIONS]\n"
+        "\n"
+        "Options:\n"
+        "  -h  Show this help message\n"
+        "  -v  Be more verbose\n"
+    );
+    exit(E_USAGE);
+}
+
+
+struct args get_args(int argc, char** argv)
+{
+    struct args args = {
+        .dummy = 0,
+    };
+
+    bool help = false;
+
+    opterr = 0; // Let me do my own error handling.
+    while (true) {
+        int c = getopt(argc, argv, "hv");
+        if (c == -1)
+            break;
+        else if (c == '?')
+            fatal(E_USAGE, "Unrecognized option '%c'", optopt);
+        else if (c == 'h')
+            help = true;
+        else if (c == 'v')
+            ++verbosity;
+    }
+
+    if (help)
+        exit_with_usage();
+
+    return args;
+}
+
+
+int main(int argc, char** argv)
+{
+    struct args args = get_args(argc, argv);
+    (void)args;
+
+    v0("Verbosity is at least 0.");
+    v1("Verbosity is at least 1.");
+    v2("Verbosity is at least 2.");
+    warning("This is a warning.");
+    fatal(E_COMMON, "This is an error.");
 }
